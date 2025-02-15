9c37736d174cd12263631a323b4185d9f02b5ab8
sajidah akther <sajidahakther@googlemail.com>
2025-02-15 12:24:16 +0000

    chore: update github workflow to run export

--- src/components/SectionItemScreen/SectionItemScreen.tsx
+++ src/components/SectionItemScreen/SectionItemScreen.tsx
@@ -3,13 +3,21 @@
 import React from "react";
 import { Text } from "@/components/Text";
 import { Layout } from "@/components/Layout";
+import { usePathname } from "next/navigation";
 import { TaskList } from "@/components/TaskList";
 
 export const SectionItemScreen = ({ sectionItem }: { sectionItem: string }) => {
-  const title = sectionItem
-    .split("-")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+  const pathname = usePathname();
+  const title =
+    process.env.NEXT_PUBLIC_EXPORT == "true"
+      ? pathname
+          .split(/[-/]+/)
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")
+      : sectionItem
+          .split("-")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ");
 
   return (
     <Layout showBack showMenu>
